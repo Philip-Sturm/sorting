ad882798fe06385c43731676c0602c2ad4c070a5
Philip <philip_sturm@outlook.de>
2026-01-19 13:53:49 +0100

    InsertSort Complete

--- insertionsort/insertionsort.go
+++ insertionsort/insertionsort.go
@@ -3,10 +3,17 @@ package insertionsort
 // MoveLeft bewegt das Element an Stelle i so lange nach links,
 // bis es an der richtigen Stelle im bereits sortierten Teil der Liste ist.
 func MoveLeft(arr []int, i int) {
-	// TODO
+	for i > 0 && arr[i] < arr[i-1] {
+		j := arr[i]
+		arr[i] = arr[i-1]
+		arr[i-1] = j
+		i--
+	}
 }
 
 // InsertionSort sortiert die übergebene Liste mittels des Insertion-Sort-Algorithmus.
 func InsertionSort(arr []int) {
-	// TODO
+	for i := 1; i < len(arr); i++ {
+		MoveLeft(arr, i)
+	}
 }
