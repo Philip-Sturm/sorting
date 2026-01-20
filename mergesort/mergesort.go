776517acf54c6e4f89ba1b87ec2f5b6f70579b4f
Philip <philip_sturm@outlook.de>
2026-01-20 21:50:16 +0100

    mergesort done

--- mergesort/mergesort.go
+++ mergesort/mergesort.go
@@ -3,12 +3,28 @@ package mergesort
 // Merge kombiniert zwei sortierte Listen zu einer sortierten Liste.
 func Merge(a1 []int, a2 []int) []int {
 	result := []int{}
-	// TODO
+	i, j := 0, 0
+	for i < len(a1) && j < len(a2) {
+		if a1[i] <= a2[j] {
+			result = append(result, a1[i])
+			i++
+		} else {
+			result = append(result, a2[j])
+			j++
+		}
+	}
+	result = append(result, a1[i:]...)
+	result = append(result, a2[j:]...)
 	return result
 }
 
 // MergeSort sortiert die übergebene Liste mittels des Merge-Sort-Algorithmus.
 func MergeSort(arr []int) []int {
-	// TODO
-	return []int{}
+	if len(arr) <= 1 {
+		return arr
+	}
+	mitte := len(arr) / 2
+	a1 := MergeSort(arr[:mitte])
+	a2 := MergeSort(arr[mitte:])
+	return Merge(a1, a2)
 }
